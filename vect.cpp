6686d2ff31734593201c5907e729e5c233a6e96c
sraphaelsohn- <sraphaelsohn@gmail.com>
2014-05-20 08:17:13 -0700

    First file

---
+++ vect.cpp
@@ -0,0 +1,24 @@
+// Compilation: c++ -std=c++0x vect.cpp
+#include <iostream>
+#include <vector>
+#include <algorithm> // includes  for_each
+
+using namespace std;
+
+class PrintInt // Functional object, or FUNCTOR.
+{
+public:    
+    void operator() (int elem) const
+    {
+        cout << elem << ' ';
+    }
+};
+
+int main( void )
+{
+    vector<int> v5(5);
+    v5 = { 1, 2, 3, 4, 5 };
+    for_each( v5.rbegin(), v5.rend(), PrintInt() );
+    cout << endl;
+}
+
